7cc2f280c1809945b7cdb11e3ea8de3f78cc82ee
Erik <147524279+ericgro2000@users.noreply.github.com>
2024-08-29 21:04:05 +0000

    Maked exact same Next.js Blog project but not instead of fetch from json server it uses actual database

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,10 +1,18 @@
-import { unstable_cache } from "next/cache";
-import { cache } from "react";
 import prisma from "./db";
 
-export const getUsers = unstable_cache(
-  cache(async () => {
-    return prisma.user.findMany();
-  }),
-  ["users"]
-);
+export async function getUsers() {
+  await wait(2000);
+
+  return prisma.user.findMany();
+}
+
+export async function getUser(userId: string | number) {
+  await wait(2000);
+  return prisma.user.findUnique({ where: { id: Number(userId) } });
+}
+
+function wait(duration: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+}
